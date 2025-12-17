2303b36620cda4d4161e86b61b4f31a4e8d8ac43
Nithin S R <nithin006600@gmail.com>
2025-12-18 02:02:46 +0530

    Use Dynamic Route Filesystem

--- server/api/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('../index'); // Import the main express app
-
-// Wrapper to ensure request/response are handled correctly
-module.exports = (req, res) => {
-    return app(req, res);
-};
